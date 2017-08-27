1917f5e579bc7e9635a7b51452dbe143d6f275ca
mansayk <6688000@gmail.com>
2017-08-27 19:06:45 +0300

    Cycling script for 'parse'

---
+++ apertium_converter_2/cycle.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+
+declare -i value=0	# Begin from
+declare -i step=100000	# Increment by
+declare -i max=2000000	# Amount of possible word forms
+declare -i m=0
+
+max=max-step
+
+if [ "$1" == "" ] ; then
+	echo "Usage: script file.txt"
+	exit 1
+elif [ ! -f "$1" ] ; then
+	echo "File '$1' not found!"
+	exit 1
+fi
+
+for i in `seq $value $step $max`; do
+	if [ $i -eq $max ] ; then
+		m=i+step
+	else
+		m=i+step-1
+	fi
+	awk '{print $2 "," $3}' $1 | ./parse -s $i -e $m > $1.ngrams.$i-$m.txt
+	#echo $i $m $max
+done    
+
