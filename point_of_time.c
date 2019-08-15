0992a6c57efd55300f74110a3c7b9201509be234
mansayk <6688000@gmail.com>
2019-08-15 22:02:13 +0300

    More additions to the previous commit

--- point_of_time.c
+++ point_of_time.c
@@ -15,14 +15,14 @@ void time_start(struct timeval *tv)
  */
 long time_stop(struct timeval *tv)
 {
-  struct timeval tv2, dtv;
-  gettimeofday(&tv2, NULL);
-  dtv.tv_sec = tv2.tv_sec  - tv->tv_sec;
-  dtv.tv_usec = tv2.tv_usec - tv->tv_usec;
-  if(dtv.tv_usec < 0) {
-	  dtv.tv_sec--;
-	  dtv.tv_usec += 1000000;
-  }
-  return (dtv.tv_sec * 1000) + (dtv.tv_usec / 1000);
+	struct timeval tv2, dtv;
+	gettimeofday(&tv2, NULL);
+	dtv.tv_sec = tv2.tv_sec - tv->tv_sec;
+	dtv.tv_usec = tv2.tv_usec - tv->tv_usec;
+	if(dtv.tv_usec < 0) {
+		dtv.tv_sec--;
+		dtv.tv_usec += 1000000;
+	}
+	return (dtv.tv_sec * 1000) + (dtv.tv_usec / 1000);
 }
 
