0f2b314442d141c096dd2887a35d13601d9d414c
mansayk <6688000@gmail.com>
2019-08-15 21:57:59 +0300

    Minor changes in point_of_time source and header files

--- point_of_time.c
+++ point_of_time.c
@@ -1,21 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <sys/time.h>
 #include "point_of_time.h"
 
-void time_start(struct timeval *tv) { gettimeofday(tv, NULL); }
+/*
+ * Get start time
+ */
+void time_start(struct timeval *tv) {
+	gettimeofday(tv, NULL);
+}
 
+/*
+ * Get finish time and calculate the difference
+ */
 long time_stop(struct timeval *tv)
 
 {
-  struct timeval tv2,dtv;
+  struct timeval tv2, dtv;
   gettimeofday(&tv2, NULL);
-
-  dtv.tv_sec= tv2.tv_sec  - tv->tv_sec;
-
-  dtv.tv_usec=tv2.tv_usec - tv->tv_usec;
-
-  if(dtv.tv_usec<0) { dtv.tv_sec--; dtv.tv_usec+=1000000; }
-
-  return dtv.tv_sec*1000+dtv.tv_usec/1000;
+  dtv.tv_sec = tv2.tv_sec  - tv->tv_sec;
+  dtv.tv_usec = tv2.tv_usec - tv->tv_usec;
+  if(dtv.tv_usec < 0) {
+	  dtv.tv_sec--;
+	  dtv.tv_usec += 1000000;
+  }
+  return (dtv.tv_sec * 1000) + (dtv.tv_usec / 1000);
 }
 
--- point_of_time.h
+++ point_of_time.h
@@ -1,8 +1,16 @@
-#ifndef _point_of_time_h_
-#define _point_of_time_h_ 1
+#ifndef _POINT_OF_TIME_H_
+#define _POINT_OF_TIME_H_ 1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void time_start(struct timeval *);
 long time_stop(struct timeval *);
 
+#ifdef __cplusplus
+}
 #endif
+
+#endif /* _POINT_OF_TIME_H_ */
 
